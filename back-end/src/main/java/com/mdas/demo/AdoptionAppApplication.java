27b6d5b6ce7dc66bf9f70efd93c7fa08f5d6e72b
iarina <cristeaiarina03@gmail.com>
2021-11-19 10:07:54 +0200

    init config

---
+++ back-end/src/main/java/com/mdas/demo/AdoptionAppApplication.java
@@ -0,0 +1,13 @@
+package com.mdas.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdoptionAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdoptionAppApplication.class, args);
+	}
+
+}
